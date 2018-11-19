6953be7bee74e1f017b80250226307ed3be2a4b2
kobora <32862953+kobora@users.noreply.github.com>
2018-11-19 16:52:12 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,1 +1,26 @@
-for(var i= 1
+var separate_time = function(time){
+var sec = Math.floor((time / 1000) % 60);
+var min = Math.floor((time / 1000 / 60) % 60);
+var hours = Math.floor((time / 1000 / 60 / 60) % 24;
+var days = Math.floor(time / 1000 / 60 / 60 / 24);
+return [sec,min,hours, days];
+}
+
+var update = function(){
+var now = new Date();
+var target = new Date();
+var diff = target.getTime() - now.getTime();
+var counter = separate_time(diff);
+document.getElementById("countdown").textContent =
+"東京オリンピックまであと"+
+counter[3] + "日" +
+counter[2] + "時間" +
+counter[1] + "分" +
+counter[0] + "秒" +
+refresh();
+}
+
+var refresh= function(){
+setTimeout(update, 1000);
+}
+update();
